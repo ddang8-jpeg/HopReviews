04a17ba7668c50fadae6afd1d891fa0dc1e3251d
Amir Modaressanavi <smodare1@jhu.edu>
2023-04-06 14:20:40 -0400

    added back button to profile page

--- HopReviews/app/src/main/java/com/example/hopreviews/ProfileActivity.java
+++ HopReviews/app/src/main/java/com/example/hopreviews/ProfileActivity.java
@@ -1,14 +1,32 @@
 package com.example.hopreviews;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.hopreviews.databinding.ActivityProfileBinding;
+import com.example.hopreviews.databinding.ActivitySignUpBinding;
 
 public class ProfileActivity extends AppCompatActivity {
+    private ActivityProfileBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        binding = ActivityProfileBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        this.finish();
+        return super.onOptionsItemSelected(item);
     }
 }
