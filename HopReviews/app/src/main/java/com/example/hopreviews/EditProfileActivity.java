6d8f4ed199c920b1bd646da7a3074dfed39fa6a0
Mihir2000 <mihir.chakravarthi@gmail.com>
2023-04-08 04:03:31 -0400

    begun profile functionality

---
+++ HopReviews/app/src/main/java/com/example/hopreviews/EditProfileActivity.java
@@ -0,0 +1,7 @@
+package com.example.hopreviews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+}
--- HopReviews/app/src/main/java/com/example/hopreviews/ProfileActivity.java
+++ HopReviews/app/src/main/java/com/example/hopreviews/ProfileActivity.java
@@ -4,13 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.hopreviews.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
     private ActivityProfileBinding binding;
+    private ImageView editProfileIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,35 @@ public class ProfileActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+//        editProfileIcon = findViewById(R.id.edit_profile);
+//        editProfileIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                startEditProfileActivity();
+//            }
+//        });
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        this.finish();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.edit_profile, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.edit_profile) {
+            Intent intent = new Intent(this, EditProfileActivity.class);
+            startActivity(intent);
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void startEditProfileActivity() {
+        Intent intent = new Intent(this, EditProfileActivity.class);
+        startActivity(intent);
     }
 }
